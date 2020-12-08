bd26c364b2ae0fa39c1d020951c81984a4f68e92
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2020-12-08 19:45:26 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
 function start(){
-    document.getElementById("textbox").innerHTML = ""
+document.getElementById("textbox").innerHTML = ""
     recognition.start();
 }
 recognition.onresult = function run(event){
@@ -9,18 +9,25 @@ recognition.onresult = function run(event){
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
+    if(Content == "take my selfie"){
+        console.log("Taking selfie in 5 seconds");
     speak();
+    }
 }
 
 
 
 
 function speak(){
+    
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis); 
-    Webcam.attach(camera);                                                                                                                                                                                                                                                          
+    Webcam.attach(camera);
+    setTimeout(function(){
+    take_snapshot();
+    },5000);                                                                                                                                                                                                                                                         
 }
 
 
@@ -32,3 +39,20 @@ image_format: 'png',
 png_quality:90
 }); 
 camera = document.getElementById("camera");
+
+
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id = "selfie_image" src = "' + data_uri + '">';
+    });
+}
+
+
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
